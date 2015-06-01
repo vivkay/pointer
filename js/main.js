8feabedc94722550aba69f384482c79503097b2d
Vivienne Kay <vivkay8@gmail.com>
2015-05-31 22:24:26 -0400

    added waypoint animation to phone1
    (added placeholder test for billboard slider)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 var $logoBtnOff = $('.logo-btn-off');
 var $panelWrapper =('.panel-wrapper');
+var $phone1 = $('.phone1');
+
 
 $logoBtnOff.on('click', function () {
-    $logoBtnOff.toggleClass('logo-btn-on', 'js-panel-wrapper-open');
+    $logoBtnOff.toggleClass('logo-btn-on');
+    $panelWrapper.toggleClass('js-panel-wrapper-open');
 });
 
-
+$phone1.waypoint(function (direction) {
+    if (direction == 'down') {
+        $phone1.addClass('js-left-in');
+    } else { 
+        $phone1.removeClass('js-left-in');
+    }
+}, { offset:'50%'});
